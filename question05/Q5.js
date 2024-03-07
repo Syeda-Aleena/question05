61e6300f12540d90a6f89c1685d1300de2717835
Syeda Aleena <159823689+Syeda-Aleena@users.noreply.github.com>
2024-03-08 02:41:59 +0500

    Add files via upload

---
+++ question05/Q5.js
@@ -0,0 +1,3 @@
+var famousPerson = ("William Shakespeares");
+var message = ("".concat(famousPerson, " said, \"Listen to many, speak to a few\"."));
+console.log("".concat(famousPerson, " said, \"Listen to many, speak to a few\"."));
---
+++ question05/Q5.ts
@@ -0,0 +1,3 @@
+let famousPerson: string = ("William Shakespeares");
+let message: string =(`${famousPerson} said, "Listen to many, speak to a few".`);
+console.log(`${famousPerson} said, "Listen to many, speak to a few".`);
